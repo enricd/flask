78fe7d7d9f65b2986061187a2f69ac38f4110b5d
enricd <edomingodomenech@gmail.com>
2021-02-20 00:30:18 +0100

    implementing the ML CV model to the server

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS     # this will avoid some problems with permissions
+import torch
+import numpy as np
+from PIL import Image
 
 app = Flask(__name__)
 CORS(app)
+
+model = torch.jit.load('model.zip')
 
 @app.route('/')
 def hello_world():
     return 'Hello, World'
 
-@app.route('/predict')
+@app.route('/predict', methods=['POST'])
 def predict():
-    return 'predictions'
+    # load image
+    img = Image.open(request.files['file'].stream).convert(
+        'RGB').resize((224,224))
+    img = np.array(img)
+    img = torch.FloatTensor(img.transpose((2,0,1)) / 255)
 
+    # get predictions
+    preds = model(img.unsqueeze(0)).squeeze()
+    probas = torch.softmax(preds, axis=0)
+    ix = torch.argmax(probas, axis=0)
+
+    return{
+        'label': model.labels[ix],
+        'score': probas[ix].item()
+    }
 
 if __name__ == "__main__":
     app.run()
